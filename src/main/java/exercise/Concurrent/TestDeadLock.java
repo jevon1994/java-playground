e8abba636e2534ae6ac57e0e412486986dc1b386
mujian <mujian@panhaidata.com>
2018-09-28 11:34:33 +0800

    死锁写法

--- src/main/java/exercise/Concurrent/TestDeadLock.java
+++ src/main/java/exercise/Concurrent/TestDeadLock.java
@@ -1,51 +1,38 @@
 package exercise.Concurrent;
 
-public class TestDeadLock implements Runnable {
-    private int flag = 1;
-    //加载完静态代码块后再加载
-    static Object A = new Object(), B = new Object();
 
-    @Override
-    public void run() {
+class TestDeadLock {
 
-        if (flag == 1) {
-            //锁定o1对象
-            synchronized (A) {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                //锁定o2对象
-                synchronized (B) {
-                    System.out.println(Thread.currentThread().getName());
-                }
-            }
-        }
-        if (flag == 0) {
-            //锁定o2对象
-            synchronized (B) {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                //锁定o1对象
-                synchronized (A) {
-                    System.out.println(Thread.currentThread().getName());
-                }
-            }
-        }
-    }
+    static Book book1 = new Book();
+    static Book book2 = new Book();
 
     public static void main(String[] args) {
-        TestDeadLock tt1 = new TestDeadLock();
-        TestDeadLock tt2 = new TestDeadLock();
-        tt1.flag = 0;
-        tt2.flag = 1;
-        Thread t1 = new Thread(tt1);
-        Thread t2 = new Thread(tt2);
-        t1.start();
-        t2.start();
+        new Thread(() -> {
+            try {
+                synchronized (book1) {
+                    Thread.sleep(1000);
+                    book2.getBook();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                synchronized (book2) {
+                    Thread.sleep(1000);
+                    book1.getBook();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
+
+class Book {
+    public synchronized Book getBook() {
+        return new Book();
+    }
+}
+
