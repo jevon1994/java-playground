387fa95e71cd2eeed511938e500f2ad1b919e1e1
jevon <m_jevon@163.com>
2019-01-20 17:44:43 +0800

    Singleton

---
+++ src/main/java/com/exercise/DesignPattern/creation/Singleton.java
@@ -0,0 +1,15 @@
+package com.exercise.DesignPattern.creation;
+
+public class Singleton {
+
+    private static final class inner{
+        private static final Singleton instance = new Singleton();
+    }
+
+    private Singleton() {
+    }
+
+    private static Singleton getInstance(){
+        return inner.instance;
+    }
+}
