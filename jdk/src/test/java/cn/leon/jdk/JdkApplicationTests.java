03af75ae7ef2bb97226c2f160691434d191c7356
mujian <m_jevon@163.com>
2019-10-29 11:16:16 +0800

    feat: 新增jdk源码环境

---
+++ jdk/src/test/java/cn/leon/jdk/JdkApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.leon.jdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
