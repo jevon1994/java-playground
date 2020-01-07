53d71cf147cd76a20813d7ec01191c9a12c13915
mujian <m_jevon@163.com>
2020-01-07 11:03:39 +0800

    feat: Prototype

---
+++ basic/src/main/java/com/design_parttern/creation/PrototypeTest.java
@@ -0,0 +1,45 @@
+package com.design_parttern.creation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName PrototypeTest
+ * @Description
+ * @Author Jevon
+ * @Date2020/1/7 9:02
+ **/
+@Slf4j
+public class PrototypeTest {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Prototype prototypeA = new Prototype("A", new HashMap<>());
+        Prototype prototypeB = prototypeA.clone();
+        log.info("A:{},B:{},result:{}", prototypeA, prototypeB, prototypeA.getMap() == prototypeB.getMap());
+    }
+}
+
+/**
+ * 浅拷贝
+ * 对象内的引用也被复制时为深拷贝
+ */
+@Data
+@AllArgsConstructor
+class Prototype implements Cloneable {
+    private String name;
+    private Map<String, String> map;
+
+
+    @Override
+    protected Prototype clone() throws CloneNotSupportedException {
+        Prototype prototype = (Prototype) super.clone();
+        prototype.map = map;
+        prototype.name = name;
+        return prototype;
+    }
+}
+
+
